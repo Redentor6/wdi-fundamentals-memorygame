839287509e1b5b1f515359400849982f0dd0a8cb
Bradford Muron <bradfordmuron@Bradfords-MBP.local>
2017-12-28 13:00:35 -0500

    memory game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,18 +6,23 @@ console.log("User flipped " + cardTwo);*/
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = [0];
-var cardTwo = [2];
-
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-
-console.log("User flipped queen")
-console.log("User flipped king")
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!")
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
 }
-else {
-	alert("Sorry, try again!")
+	else {
+	alert("Sorry, try again!");
 }
+
+};
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
