1868caa5c29c4e1d1bf01452ceef75748fc193bc
Bradford Muron <bradfordmuron@Bradfords-MBP.local>
2017-12-29 00:13:02 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,7 +3,29 @@
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);*/
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-king.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -15,13 +37,11 @@ var checkForMatch = function() {
 }
 
 };
-
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-
-	cardsInPlay.push(cards[cardId]);
-
-	checkForMatch();
+	var flipCard = function(cardId) {
+		console.log("user flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 };
 
 flipCard(0);
